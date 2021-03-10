7c6e570f5e03efb02348638eebe7bfcd6f72a215
Dao Luong <daovanluongpdl@gmail.com>
2021-03-10 10:54:54 +0700

    filter and sort products

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Filter from './components/Filter';
 import Products from './components/Products';
 import data from './data.json';
 
@@ -11,6 +12,58 @@ class App extends React.Component {
       sort: '',
     };
   }
+
+  filterProducts = (size) => {
+    this.setState((state) => {
+      const { sort } = this.state;
+      const newProducts =
+        size === ''
+          ? data.products.slice().sort((a, b) => {
+              switch (sort) {
+                case 'lowest':
+                  return a.price - b.price;
+                case 'highest':
+                  return b.price - a.price;
+
+                default:
+                  return b.id - a.id;
+              }
+            })
+          : data.products
+              .filter((product) => product.availableSizes.indexOf(size) >= 0)
+              .slice()
+              .sort((a, b) => {
+                switch (sort) {
+                  case 'lowest':
+                    return a.price - b.price;
+                  case 'highest':
+                    return b.price - a.price;
+
+                  default:
+                    return b.id - a.id;
+                }
+              });
+      return { ...state, size, products: newProducts };
+    });
+  };
+
+  sortProducts = (sort) => {
+    this.setState((state) => {
+      const newProducts = state.products.slice().sort((a, b) => {
+        switch (sort) {
+          case 'lowest':
+            return a.price - b.price;
+          case 'highest':
+            return b.price - a.price;
+
+          default:
+            return b.id - a.id;
+        }
+      });
+      return { ...state, sort, products: newProducts };
+    });
+  };
+
   render() {
     const { products, size, sort } = this.state;
     return (
@@ -21,6 +74,12 @@ class App extends React.Component {
         <main>
           <div className='content'>
             <div className='main'>
+              <Filter
+                count={products.length}
+                {...this.state}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+              />
               <Products products={products} />
             </div>
             <div className='sidebar'>Cart Items</div>
